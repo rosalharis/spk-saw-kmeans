575fc9b540a581cf7b08d245e8cfbf7e5186e002
Rosal Haris <90707185+rosalharis@users.noreply.github.com>
2024-10-24 16:33:08 +0700

    Delete spk-saw directory

--- spk-saw/alternatif/aksi_hapus_alternatif.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../koneksi.php";
-$sql="delete from tbl_alternatif where id_alternatif='$_GET[id_alternatif]'";
-mysqli_query($koneksi,$sql);
-//echo "$sql";
-header("location:../index.php?halaman=data");
-?>
--- spk-saw/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-$koneksi=mysqli_connect("localhost","root","","db_spk");
-mysqli_select_db($koneksi,"db_spk") or die("Gagal Terhubung ke database!");
-?>
